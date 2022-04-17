87ffd09a6cce25c27bb0e23fc958363604f1edb6
Vedant Chak <40168412+ved7@users.noreply.github.com>
2022-04-18 03:46:23 +0530

    Create Product_of_Array_Except_Self.cpp

---
+++ Product_of_Array_Except_Self.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> prevMultiple(n);
+        vector<int> nextMultiple(n);
+        prevMultiple[0] = 1;
+        nextMultiple[n - 1] = 1;
+        for (int i = 1; i < n; i++)
+        {
+            prevMultiple[i] = prevMultiple[i - 1] * nums[i - 1];
+        }
+        for (int i = n - 2; i >= 0; i--)
+        {
+            nextMultiple[i] = nextMultiple[i + 1] * nums[i + 1];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            prevMultiple[i] = prevMultiple[i] * nextMultiple[i];
+        }
+        return prevMultiple;
+    }
+};
