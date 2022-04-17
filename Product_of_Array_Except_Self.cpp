f5f404ce263e88d5ec230556c8d6ef10c6ef5fd4
Vedant Chak <40168412+ved7@users.noreply.github.com>
2022-04-18 03:50:16 +0530

    Update Product_of_Array_Except_Self.cpp

--- Product_of_Array_Except_Self.cpp
+++ Product_of_Array_Except_Self.cpp
@@ -1,5 +1,6 @@
 // Approach: maintain two array prevMultiple which contain multiple of all previous elements in array and like wise 
 // nextMultiple for all element occuring after(excluding itself in both cases) and finally resultant element will be next[index] * prev[index]
+// Time complexity: O(n) and Space Complexity: O(n)
 class Solution
 {
 public:
