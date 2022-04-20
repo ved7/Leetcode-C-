c4e0bc421c657ffbbb32a6dffe0a01937c8be873
Vedant Chak <40168412+ved7@users.noreply.github.com>
2022-04-21 01:57:42 +0530

    Create Two_Sum_II.cpp

---
+++ Two_Sum_II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        int l = 0;
+        int r = n-1;
+        
+        while(l<r){
+            int sum = numbers[l] + numbers[r];
+            if(sum == target){
+                return {l+1,r+1};
+            }
+            if(sum > target){
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        return {};
+    }
+};
