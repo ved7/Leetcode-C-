12579dc1a2037d116cccfc3cbf6909c2f7582c65
Vedant Chak <40168412+ved7@users.noreply.github.com>
2022-04-30 21:22:02 +0530

    Create Min_sorted_array.cpp

---
+++ Min_sorted_array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int res = nums[0];
+        
+        while( l <= r) {
+          if(nums[l] <= nums[r]){
+              res = min(res,nums[l]);
+              break;
+          }
+            int mid = (l + r) /2;
+            res = min(res, nums[mid]);
+            if(nums[l] <= nums[mid]){
+                l = mid + 1;
+            }
+            else{
+                r = mid -1;
+            }
+        }
+        return res;
+    }
+};
