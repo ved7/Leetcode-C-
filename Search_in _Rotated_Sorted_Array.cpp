94000db83d4621005a7a98446277598b8d147ac3
Vedant Chak <40168412+ved7@users.noreply.github.com>
2022-04-30 20:56:00 +0530

    Create Search_in _Rotated_Sorted_Array.cpp

---
+++ Search_in _Rotated_Sorted_Array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        
+        while( l <= r ) {
+            int mid = (l + r) / 2 ;
+            if(nums[mid] == target) return mid ;
+            
+            //left array
+            if(nums[l] <= nums[mid]) {
+                if(target > nums[mid] || target < nums[l]) l = mid + 1 ;
+                else r = mid -1;
+            }
+            //right array
+            else{
+                if(target < nums[mid] || target > nums[r]) r = mid - 1;
+                else l = mid + 1 ; 
+            }
+            
+        }
+        return -1;
+    }
+};
